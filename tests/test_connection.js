eaa08039b39768e8d6364d0aaf0a609dc24f1f48
Craig Norman <cnorman@gasbuddy.com>
2017-12-20 09:24:23 -0600

    Add the ability to provide multiple hosts to elastic search client.

--- tests/test_connection.js
+++ tests/test_connection.js
@@ -12,3 +12,16 @@ tap.test('test_connection', async (t) => {
   t.ok(elastic, 'Should have a connect method');
   await elastic.stop();
 });
+
+tap.test('test_multiple_connection', async (t) => {
+  const config = {
+    name: 'test-elastic-multiple',
+    hosts: [
+      process.env.ELASTIC_URL || 'http://elastic:9200'
+    ]
+  }
+  const elastic = new Elastic(winston, config);
+  await elastic.start({});
+  t.ok(elastic, 'Should have connect method');
+  await elastic.stop();
+});
